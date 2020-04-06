fc1a3eb657df5bd5ddfaf34e9c2ec180f3538b25
Vincent <vincent.guillamot@laposte.net>
2020-04-07 01:35:29 +0200

    Ajout du model du Player

---
+++ src/app/model/player.ts
@@ -0,0 +1,17 @@
+export interface IPlayer {
+  profile_id: string;
+  steam_id: string;
+  name: string;
+  clan: string;
+  country: string;
+  slot: number;
+  slot_type: number;
+  rating: number;
+  games: number;
+  wins: number;
+  streak: number;
+  drops: number;
+  color: string;
+  team: string;
+  civ: string;
+}
